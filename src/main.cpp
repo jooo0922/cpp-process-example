58126fcf93e1fdcccc937a4f2c1ab3422c5ec495
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2024-09-17 22:19:08 +0900

    refactor: 각 프로세스에서 printf() 호출 함수 추출

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
+void foo() { printf("execute foo\n"); }
+
 int main() {
-  // 현재 실행되는 프로그램의 프로세스(= 부모 프로세스) PID 출력
-  printf("parent pid is %d\n", getpid());
 
   /*
     fork()
@@ -21,9 +21,15 @@ int main() {
       자식 프로세스의 PID 출력
     */
     printf("child pid is %d\n", getpid());
+    foo();
+  } else {
+    /*
+      현재 실행 중인 프로세스가 부모 프로세스임을 검사한 후,
+      부모 프로세스의 PID 출력
+    */
+    printf("parent pid is %d\n", getpid());
+    foo();
   }
-
-  printf("execute!\n");
 
   return 0;
 }
